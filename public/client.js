603d1921aefa3a66303cdb0a455a09dcda6343df
TotoroTron <brian.cheng.1997@gmail.com>
2023-11-15 21:32:30 -0500

    holy shet it works! data is now fetched and rendered by client-side! currently working with just 1 review on screen

--- public/client.js
+++ public/client.js
@@ -1,20 +1,26 @@
-// async function fetchReviewInfo(reviewID) {
-//     //const username = document.getElementById('reviewID').value;
-//     const apiUrl = `http://localhost:8080/api/reviews/getreview?_id=${reviewID}`;
-    
-//     try {
-//         const response = await fetch(apiUrl);
-        
-//         if (!response.ok) {
-//             throw new Error('Failed to fetch review information');
-//         }
-
-//         const data = await response.json();
-//         document.getElementById('displayUserID').textContent = data.user_id || 'N/A';
-//         document.getElementById('displayReviewDate').textContent = data.review_date || 'N/A';
-//         document.getElementById('displayReviewText').textContent = data.review_text || 'N/A';
-
-//     } catch (error) {
-//         alert(error.message);
-//     }
-// }
+function fetchReviewDetails(reviewId) {
+    const url = new URL('/api/reviews/getreview', 'http://localhost:8080');
+    url.searchParams.append('review_id', reviewId);
+  
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Here you would use the data to populate your custom element or another part of your page
+        console.log(data);
+        // For example, if you had a div with an id of 'review-container' where you want to display the review
+        const reviewContainer = document.getElementById('customReviewsContainer');
+        const userReview = document.createElement('user-review');
+        userReview.setAttribute('user-id', data.user_id);
+        userReview.setAttribute('review-date', data.review_date);
+        userReview.setAttribute('review-text', data.review_text);
+        reviewContainer.appendChild(userReview);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }
